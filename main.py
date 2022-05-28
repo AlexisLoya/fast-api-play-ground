c016a25b9cf5ac31c2f905ae4157ad0944bba523
AlexisLoya <alexloy117@gmail.com>
2022-05-28 18:33:57 -0500

    validations_models

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Python
 from operator import gt
 from typing import Optional
+from enum import Enum
 
 # Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI, Path, Query
@@ -11,17 +13,48 @@ from fastapi import Body
 app = FastAPI()
 
 # Models
+
+class HairColor(Enum):
+    white = "White",
+    brown = "brown",
+    black = "black",
+    blonde = "blonde",
+    red = "red",
 class Location(BaseModel):
-    city: str
-    state: str
-    country: str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        )
 
 class Person(BaseModel):
-    firs_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None 
-    is_married: Optional[bool] = None
+    firs_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=130,
+        )
+    hair_color: Optional[HairColor] = Field(default=None) 
+    is_married: Optional[bool] = Field(default=None)
 
 
 @app.get("/")
