dd52e8754d5198aedd7403f46a2796e3c22c9bf1
AlexisLoya <alexloy117@gmail.com>
2022-05-30 11:12:41 -0500

    summary_and_description

--- main.py
+++ main.py
@@ -85,9 +85,15 @@ class LoginOut(BaseModel):
 @app.get(
     path="/",
     status_code=status.HTTP_200_OK,
-    tags=['Home']
+    tags=['Home'],
+    summary="Hello World with FastAPI"
     )
 def home():
+    """AI is creating summary for home
+
+    Returns:
+        [type]: [{"Hello": "World"}]
+    """
     return {"Hello": "World"}
 
 # Request and Response Body
@@ -95,9 +101,18 @@ def home():
     path="/person/new/",
     status_code=status.HTTP_201_CREATED, 
     response_model=PersonOut,
-    tags=['Persons']
+    tags=['Persons'],
+    summary="Create a new person in the app"
     )
 def new_person(person: Person = Body(...)):
+    """AI is creating summary for new_person
+
+    Args:
+        person (Person, optional): [description]. Defaults to Body(...).
+
+    Returns:
+        [type]: [New person]
+    """
     return person
 
 
@@ -106,7 +121,8 @@ def new_person(person: Person = Body(...)):
 @app.get(
     path="/person/detail",
     status_code=status.HTTP_200_OK,
-    tags=['Persons'] 
+    tags=['Persons'],
+    summary="Get a person from the database"
     )
 def show_person(
     name : Optional[str] = Query(
@@ -123,6 +139,15 @@ def show_person(
         description="This is the person age. It's required"
         )
 ):
+    """AI is creating summary for show_person
+
+    Args:
+        name (Optional[str], optional) [description].
+        age (int, optional): [description].
+
+    Returns:
+        [type]: [new Person]
+    """
     return {'name':name,'age':age}
 
 
@@ -131,7 +156,8 @@ persons = [1,2,3,4,5,117]
 @app.get(
     path="/person/detail/{person_id}",
     status_code=status.HTTP_200_OK,
-    tags=['Persons']
+    tags=['Persons'],
+    summary="Get a person from the database by ID"
     )
 def show_person(
     person_id: int = Path(
@@ -142,6 +168,14 @@ def show_person(
         description="This is the person id. It's required"
         )
 ):
+    """AI is creating summary for show_person
+
+    Args:
+        person_id (int, optional): [description]."This is the person id. It's required" ).
+
+    Raises:
+        HTTPException: [description]
+    """
     if person_id not in persons:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -155,7 +189,8 @@ def show_person(
 @app.put(
     path="/person/{person_id}",
     status_code=status.HTTP_200_OK,
-    tags=['Persons']
+    tags=['Persons'],
+    summary="Update a person"
     )
 def update_person(
     person_id : int = Path(
@@ -169,13 +204,16 @@ def update_person(
         title='The person to update',
         description='data to update'
     ),
-    # location : Location = Body(
-    #     ...,
-    #     title='The person to update',
-    #     description='data to update'
-    # )
-    
-):  
+):
+    """AI is creating summary for update_person
+
+    Args:
+        person_id (int, optional): [description].
+        person (Person, optional): [description].
+
+    Returns:
+        [type]: [Person]
+    """
     response = person.dict()
     # response.update(location.dict())
     return response
@@ -187,12 +225,22 @@ def update_person(
     path="login",
     response_model=LoginOut,
     status_code=status.HTTP_200_OK,
-    tags=['Authenticate']
+    tags=['Authenticate'],
+    summary="Login in the app"
 )
 def login(
     username: str = Form(...),
     password: SecretStr = Form(...)
 ):
+    """AI is creating summary for login
+
+    Args:
+        username (str, optional): 
+        password (SecretStr, optional):
+
+    Returns:
+        [type]: [User authenticated]
+    """
     return LoginOut
 
 # Cookies and Headers Parameters
@@ -200,7 +248,8 @@ def login(
 @app.post(
     path="/contact/",
     status_code=status.HTTP_200_OK,
-    tags=['Persons']
+    tags=['Persons'],
+    summary="Get on touch with the organization"
 )
 def contact(
     fisrt_name: str = Form(
@@ -221,17 +270,39 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
     
 ):
+    """AI is creating summary for contact
+
+    Args:
+        fisrt_name (str, optional): [description].
+        last_name (str, optional): [description]. 
+        email (EmailStr, optional): [description]. 
+        message (str, optional): [description].
+        user_agent (Optional[str], optional): [description].
+        ads (Optional[str], optional): [description].
+
+    Returns:
+        [type]: [cookies]
+    """
     return user_agent
 
 # Files
 @app.post(
     path="/post-image",
     status_code=status.HTTP_201_CREATED,
-    tags=['Files']
+    tags=['Files'],
+    summary="Upload a Image File"
 )
 def post_image(
     image: UploadFile = File(...)
 ):
+    """AI is creating summary for post_image
+
+    Args:
+        image (UploadFile, optional): [description]. Defaults to File(...).
+
+    Returns:
+        [type]: [file]
+    """
     return {
         "Filename":image.filename,
         "Format":image.content_type,
@@ -242,11 +313,20 @@ def post_image(
 
 @app.post(
     path='/post-multi-image',
-    tags=['Files']
+    tags=['Files'],
+    summary="Upload many Image Files"
 )
 def post_image(
     images: List[UploadFile] = File(...)
 ):
+    """AI is creating summary for post_image
+
+    Args:
+        images (List[UploadFile], optional): [description]. Defaults to File(...).
+
+    Returns:
+        [type]: [List of files]
+    """
     info_images = [{
         "filename": image.filename,
         "Format": image.content_type,
